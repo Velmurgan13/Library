1e94be0d18904d728ec5a83d5b23c66929fae310
velmurgan pillai <karthikpillai1323@gmail.com>
2021-06-02 19:55:32 +0530

    initial setup done for authors route

--- server.js
+++ server.js
@@ -6,12 +6,15 @@ const app = express();
 
 
 const expHbs = require('express-handlebars');
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.engine('hbs', expHbs({extname:'hbs'}))
 app.set('view engine', 'hbs')
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}))
 
 const mongoose = require('mongoose')
 
@@ -26,6 +29,7 @@ db.on('error', error => console.log(error))
 db.once('open', () => console.log('connected to mongoose'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 
 app.listen(process.env.PORT || 3000)
